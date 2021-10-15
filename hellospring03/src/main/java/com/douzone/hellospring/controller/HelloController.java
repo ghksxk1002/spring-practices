852013eed0794b894d22facb4bf2917bcb75509f
ghksxk1002 <ghksxk1002@naver.com>
2021-10-15 16:07:01 +0900

    requestMapping practices

--- hellospring03/src/main/java/com/douzone/hellospring/controller/HelloController.java
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.douzone.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 백엔드 컨트롤러
@@ -12,9 +13,16 @@ public class HelloController {
 		return "/WEB-INF/views/hello.jsp";
 	}
 	
-	@RequestMapping("/guestbook/list")
-	public String hello2() {
-		return "/WEB-INF/views/hello.jsp";
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println(name);
+		return "/WEB-INF/views/hello2.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public String hello3(String name, Model model) {
+		model.addAttribute("name", name);
+		return "/WEB-INF/views/hello3.jsp";
 	}
 
 	
