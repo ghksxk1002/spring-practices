b2354b77cc90ff91f17277b66ffda2e037f247a4
ghksxk1002 <ghksxk1002@naver.com>
2021-10-27 12:54:02 +0900

    .

--- container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
+++ container/src/main/java/com/douzone/container/config/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,9 @@
 package com.douzone.container.config.soundsystem;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 // 스케닝 위치 알려주기
 @ComponentScan(basePackages = {"com.douzone.container.soundsystem"})
 public class CDPlayerConfig {
